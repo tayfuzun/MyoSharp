002e8212fbd1ef0815ebc400b5714cf47c7d90a3
tayfun.uzun <tayfun.uzun@magnetforensics.com>
2014-09-24 21:46:43 -0400

    Making MyoSharp .NET 2.0 compatible

--- MyoSharp/Device/VersionComponent.cs
+++ MyoSharp/Device/VersionComponent.cs
@@ -1,13 +1,6 @@
-﻿//-----------------------------------------------------------------------
-// <copyright file="VersionComponent.cs" company="Magnet Forensics®">
-//     Copyright (c) Magnet Forensics®. All rights reserved.
-// </copyright>
-//-----------------------------------------------------------------------
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace MyoSharp.Device
 {
--- MyoSharp/Program.cs
+++ MyoSharp/Program.cs
@@ -2,10 +2,8 @@
 using MyoSharp.Discovery;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace MyoSharp
 {
