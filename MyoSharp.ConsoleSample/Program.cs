2b2685e50b2461374841b86f65c3f24950d8ba45
nick.cosentino <nick.cosentino@magnetforensics.com>
2014-09-24 22:47:46 -0400

    Split Into Library and Application
    - Split the solution into a console app and a class library.
    - Modified the app config to allow .NET 4.0 support (since backward compatibility breakage between 3.5-4.0).

---
+++ MyoSharp.ConsoleSample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+using MyoSharp.Device;
+using MyoSharp.Discovery;
+
+namespace MyoSharp.ConsoleSample
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var hub = Hub.Create("com.myosharp.myoapp");
+            hub.StartListening();
+
+            hub.Paired += Hub_Paired;
+
+            Console.ReadLine();
+        }
+
+        private static void Hub_Paired(object sender, MyoEventArgs e)
+        {
+            e.Myo.PoseChange += Myo_PoseChange;
+            e.Myo.Connected += Myo_Connected;
+        }
+
+        private static void Myo_Connected(object sender, MyoEventArgs e)
+        {
+            e.Myo.Disconnected += Myo_Disconnected;
+            Console.WriteLine("Myo Connected");
+        }
+
+        private static void Myo_Disconnected(object sender, MyoEventArgs e)
+        {
+            Console.WriteLine("Oh no, looks like the Myo disconnected!");
+        }
+
+        private static void Myo_PoseChange(object sender, PoseEventArgs e)
+        {
+            Console.WriteLine("Pose Detected: " + e.Pose);
+        }
+    }
+}
---
+++ MyoSharp.ConsoleSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MyoSharp.ConsoleSample")]
+[assembly: AssemblyDescription("A sample console application using MyoSharp.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyoSharp.ConsoleSample")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("91810427-df07-4c30-a501-1e9e8f5833d6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
