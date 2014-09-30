3744f65b597fe38a0f6a7906b65526ee334c80ac
tayfuzun <tayf.uzun@gmail.com>
2014-09-29 20:15:38 -0400

    Example program to use the orientation data
    - helps understand roll, pitch and yaw

---
+++ MyoSharp.ConsoleSample/OrientationExample.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+using MyoSharp.Device;
+using MyoSharp.ConsoleSample.Internal;
+
+namespace MyoSharp.ConsoleSample
+{
+    /// <summary>
+    /// This example will show you how to hook onto the orientation events on
+    /// the Myo and pull roll, pitch and yaw values from it. In this example the 
+    /// raw vectors from the orientation event args are converted to roll, pitch and yaw
+    /// on a scale from 0 to 9, depending on the position of the myo
+    /// </summary>
+    /// <remarks>
+    /// Not sure how to use this example?
+    /// - Open Visual Studio
+    /// - Go to the solution explorer
+    /// - Find the project that this file is contained within
+    /// - Right click on the project in the solution explorer, go to "properties"
+    /// - Go to the "Application" tab
+    /// - Under "Startup object" pick this example from the list
+    /// - Hit F5 and you should be good to go!
+    /// </remarks>
+    internal class OrientationExample
+    {
+        #region Methods
+        private static void Main(string[] args)
+        {
+            // create a hub that will manage Myo devices for us
+            using (var hub = Hub.Create())
+            {
+                // listen for when the Myo connects
+                hub.MyoConnected += (sender, e) =>
+                {
+                    Console.WriteLine("Myo {0} has connected!", e.Myo.Handle);
+                    e.Myo.Vibrate(VibrationType.Short);
+                    e.Myo.OrientationDataAcquired += Myo_OrientationDataAcquired;
+                };
+
+                // listen for when the Myo disconnects
+                hub.MyoDisconnected += (sender, e) =>
+                {
+                    Console.WriteLine("Oh no! It looks like {0} arm Myo has disconnected!", e.Myo.Arm);
+                    e.Myo.OrientationDataAcquired -= Myo_OrientationDataAcquired;
+                };
+
+                // wait on user input
+                ConsoleHelper.UserInputLoop(hub);
+            }
+        }
+        #endregion
+
+        #region Event Handlers
+        private static void Myo_OrientationDataAcquired(object sender, OrientationDataEventArgs e)
+        {
+            var pi = (float)System.Math.PI;
+
+            // convert the values to a 0-9 scale (for easier digestion/understanding)
+            var roll = (int)((e.Roll + pi) / (pi * 2.0f) * 10);
+            var pitch = (int)((e.Pitch + pi) / (pi * 2.0f) * 10);
+            var yaw = (int)((e.Yaw + pi) / (pi * 2.0f) * 10);
+
+            Console.Clear();
+            Console.WriteLine(@"Roll: {0}", roll);
+            Console.WriteLine(@"Pitch: {0}", pitch);
+            Console.WriteLine(@"Yaw: {0}", yaw);
+        }
+        #endregion
+    }
+}
